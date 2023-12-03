143a5554191ce35b923c20a5c90bf7cf4f8cb09b
Harrison Bierman <harryscout@gmail.com>
2023-12-03 12:28:50 -0800

    compacted code

--- day2_part1/src/main.rs
+++ day2_part1/src/main.rs
@@ -8,7 +8,6 @@ const TARGET_BLUE: u32 = 14;
 fn main() {
     let re_color = Regex::new(r"red|green|blue").unwrap();
     let re_num = Regex::new(r"\d+").unwrap();
-    // the target game is 12 red cubes, 13 green cubes, 14 blue cubes
     let mut sum: usize = 0;
     let contents: String = fs::read_to_string("src/input.txt").expect("could not read file");
     for (game_id, game) in contents.lines().enumerate() {
@@ -18,18 +17,11 @@ fn main() {
         'subset: for subset in game.to_string().split(";") {
             for group in subset.split(",") {
                 let num = re_num.find(group).unwrap().as_str().parse::<u32>().unwrap(); 
-                //println!("{}", group);
                 match re_color.find(group).unwrap().as_str(){
-                    "red" => if num > TARGET_RED {
-                        is_possible = false
-                    },
-                    "green" => if num > TARGET_GREEN {
-                        is_possible = false
-                    },
-                    "blue" => if num > TARGET_BLUE {
-                        is_possible = false
-                    },
-                    _ => println!("Color not found"),
+                    "red" => if num > TARGET_RED { is_possible = false },
+                    "green" => if num > TARGET_GREEN { is_possible = false },
+                    "blue" => if num > TARGET_BLUE { is_possible = false },
+                    _ => (),
                 }
                 if !is_possible {
                     break 'subset;
