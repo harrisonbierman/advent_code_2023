2f62d0f4c6591fa371787714aabc41516b5e11eb
Harrison Bierman <harryscout@gmail.com>
2023-12-03 17:21:14 -0800

    finished day 2 part 2

--- day2_part2/src/main.rs
+++ day2_part2/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let re_group = Regex::new(r"(\d+)\s+(?:(red)|(green)|(blue))").unwrap();
+    let mut sum: u32 = 0;
+    let contents: String = fs::read_to_string("src/input.txt").expect("could not read file");
+    for game in contents.lines() {
+        let mut red_max: u32 = 0;
+        let mut green_max: u32 = 0;
+        let mut blue_max: u32 = 0;
+        for (_, [num, color])in re_group.captures_iter(game).map(|c| c.extract::<2>()) {
+            let num = num.parse::<u32>().unwrap();
+            match color {
+                "red" => { if num > red_max { red_max = num; } },
+                "green" => { if num > green_max { green_max = num; } },
+                "blue" => { if num > blue_max { blue_max = num; } },
+                _ => (),
+            }
+        }
+        let power_of_min: u32 = red_max * green_max * blue_max;
+        sum += power_of_min;
+    }
+    println!("the sum of all the powers is {}", sum);
 }
