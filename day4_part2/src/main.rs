7137449f9f520934eb2769011877aaaf986b91fb
Harrison Bierman <harryscout@gmail.com>
2023-12-05 12:47:42 -0800

    some progress

--- day4_part2/src/main.rs
+++ day4_part2/src/main.rs
@@ -3,11 +3,11 @@ use std::error::Error;
 use std::fs;
 use regex::{ Regex, Match };
 
-
+#[derive(Debug)]
 pub struct Card {
     pub parent: Weak<Card>,
     pub number: u32,
-    pub children: Vec<Option<Rc<Card>>>
+    pub children: Vec<Rc<Card>>
 }
 
 impl Card {
@@ -19,16 +19,20 @@ impl Card {
         }
     }
 
-    pub fn add_parent(&mut self, parent_card: &Rc<Card>) {
-        self.parent = Rc::<Card>::downgrade(parent_card);
+    pub fn add_parent(&mut self, parent_card: Rc<Card>) {
+        self.parent = Rc::<Card>::downgrade(&parent_card);
+    }
+
+    pub fn add_child(&mut self, child_card: Rc<Card>) {
+        self.children.push(child_card);
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let contents: String = fs::read_to_string("src/input.txt")?;
+    let contents: String = fs::read_to_string("src/test.txt")?;
     let re_num: Regex = Regex::new(r"\d+")?;
     let mut sum: u32 = 0;
-    for line in contents.lines() {
+    for (card_num, line) in contents.lines().enumerate() {
         let three_parts: Vec<&str> = line.split([':','|']).collect();
         let mut win_arr: Vec<u32> = Vec::new();
         for win_num in re_num.find_iter(three_parts[1]).map(|c: Match<'_>| c.as_str()) {
@@ -40,14 +44,19 @@ fn main() -> Result<(), Box<dyn Error>> {
             match_arr.push(match_num.parse::<u32>()?)
         }
 
+        let mut card = Card::new(card_num as u32 + 1);
+
         let mut win_count: u32 = 0;
         for win_num in &win_arr {
             for match_num in &match_arr {
                 if win_num == match_num {
-                    card_matches.push(match_num)
+                    card.add_child(
+                        Rc::new(Card::new(*match_num))
+                    )
                 }
             }
         }
+        println!("{:#?}", card);
 
 
     }
