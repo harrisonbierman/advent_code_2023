fa73b138a9c7109c93d1ba5e9f48050ba6105778
Harrison Bierman <harryscout@gmail.com>
2023-12-04 23:57:38 -0800

    fixed formatting

--- day1_part1/src/main.rs
+++ day1_part1/src/main.rs
@@ -5,22 +5,25 @@ fn main() {
     let mut sum: u32 = 0;
     for line in contents.lines() {
             let mut ch_total: String = String::new();
-        for ch in line.chars() {
+            for ch in line.chars() {
             match ch.to_digit(10) {
                 Some(_) => ch_total.push(ch),
                 None => (),
             }
         }
+
         if ch_total.len() == 1 {
             ch_total = ch_total.clone() + ch_total.as_str();
         }
+
         if ch_total.len() > 2 {
             let mut temp = ch_total.as_str().chars().nth(0).unwrap().to_string();
             temp.push(ch_total.as_str().chars().nth(ch_total.len() - 1).unwrap());
             ch_total = temp;
         }
+
         sum += ch_total.parse::<u32>().unwrap();
-            //println!("{}", &ch_total)
     }
+
     println!("the total is {}", sum)
 }
